b9d6c3404ab9ca8d1e7e2c1fed452c2eabc4a144
itay Zemah <itayzemah@gmail.com>
2020-09-26 19:42:57 +0300

    SuperMarketList + 2 fragments.

--- app/src/main/java/com/example/finalproject/Fragment_Super_List.java
+++ app/src/main/java/com/example/finalproject/Fragment_Super_List.java
@@ -1,10 +1,26 @@
 package com.example.finalproject;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment_Super_List extends Fragment {
     protected View view;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        Log.d("pttt","onCreateView");
+        if(view==null){
+            view = inflater.inflate(R.layout.fragment_super_list, container, false);
+        }
+        return view;
+    }
 }
