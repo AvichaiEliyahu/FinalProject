916c588df01d0a4bb37483f2a6112d97ee9e10f2
itay Zemah <itayzemah@gmail.com>
2020-09-26 19:37:30 +0300

    Add material to gradle + change theme

---
+++ app/src/main/java/com/example/finalproject/Fragment_Super_List.java
@@ -0,0 +1,10 @@
+package com.example.finalproject;
+
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment_Super_List extends Fragment {
+    protected View view;
+
+}
