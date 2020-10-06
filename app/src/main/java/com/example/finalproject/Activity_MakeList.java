4729c4871e66d29253250e3fa0948fcac2b4d1d8
AvichaiEliyahu <avihayboom@gmail.com>
2020-10-06 18:59:05 +0300

    added key for Activity_MakeList.java

--- app/src/main/java/com/example/finalproject/Activity_MakeList.java
+++ app/src/main/java/com/example/finalproject/Activity_MakeList.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class Activity_MakeList extends AppCompatActivity {
 
+    public static final String SUPER_ID = "SUPER_ID";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_list);
+
     }
 }
