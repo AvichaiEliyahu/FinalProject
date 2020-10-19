4303d19540b0030334f5f071e686293a491ba7f0
itay Zemah <itayzemah@gmail.com>
2020-10-19 15:30:43 +0300

    End_Activity fix

--- app/src/main/java/com/example/finalproject/End_Activity.java
+++ app/src/main/java/com/example/finalproject/End_Activity.java
@@ -7,10 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.button.MaterialButton;
+
 public class End_Activity extends AppCompatActivity {
 
-    private Button end_BTN_newList;
-    private Button end_BTN_exit;
+    private MaterialButton end_BTN_newList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,21 +24,11 @@ public class End_Activity extends AppCompatActivity {
         end_BTN_newList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(End_Activity.this,Activity_Supermarket_Select.class);
-                startActivity(i);
-                finish();
-            }
-        });
-        end_BTN_exit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
                 finish();
             }
         });
     }
-
     private void findViews() {
         end_BTN_newList = findViewById(R.id.end_BTN_newList);
-        end_BTN_exit = findViewById(R.id.end_BTN_exit);
     }
 }
